3c2bdbf7fe0d4826ed78011f56e0c4dc2f3e076a
Akio Nkodia <rnanimo@gmail.com>
2020-04-06 14:20:50 +0300

    - add table with sticky columns

--- angular-site/src/main.ts
+++ angular-site/src/main.ts
@@ -133,6 +133,7 @@ import {TableSelectionExample} from './app/data-table/table/table-selection-exam
 import {TableMultipleHeaderFooterExample} from './app/data-table/table/table-multiple-header-footer-example';
 import {TableMultipleHeaderFooterExample} from './app/data-table/table/table-multiple-header-footer-example';
 import {TableOverviewExample} from './app/data-table/table/table-overview-example';
+import {TableStickyColumnsExample} from './app/data-table/table/table-sticky-columns-example';
 
 // Default MatFormField appearance to 'fill' as that is the new recommended approach and the
 // `legacy` and `standard` appearances are scheduled for deprecation in version 10.
@@ -162,7 +163,7 @@ let components: any[] =
       SnackBarComponentExample, PizzaPartyComponent, TooltipCustomClassExample, TooltipDelayExample, TooltipMessageExample, TooltipManualExample,
       TooltipAutoHideExample,
       PaginatorConfigurableExample, SortOverviewExample, TableBasicExample, TableBasicFlexExample, TableDynamicColumnsExample,
-      TableExpandableRowsExample, TableSelectionExample, TableMultipleHeaderFooterExample, TableOverviewExample];
+      TableExpandableRowsExample, TableSelectionExample, TableMultipleHeaderFooterExample, TableOverviewExample, TableStickyColumnsExample];
 
 @NgModule({
   imports: [
@@ -199,7 +200,7 @@ let components: any[] =
                    BottomSheetOverviewExample, DialogOverviewExample, SnackBarOverviewExample, SnackBarComponentExample, TooltipCustomClassExample,
                    TooltipDelayExample, TooltipMessageExample, TooltipManualExample, TooltipAutoHideExample,
                    PaginatorConfigurableExample, SortOverviewExample, TableBasicExample, TableBasicFlexExample, TableDynamicColumnsExample,
-                   TableExpandableRowsExample, TableSelectionExample, TableMultipleHeaderFooterExample, TableOverviewExample],
+                   TableExpandableRowsExample, TableSelectionExample, TableMultipleHeaderFooterExample, TableOverviewExample, TableStickyColumnsExample],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
   ]
