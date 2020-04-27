a49c575c73fe0ecee27ff7d230a43402e76ab0d2
Dmitrii Shadrin <Dmitrii_Shadrin@externals.adidas.com>
2020-04-27 16:12:16 +0300

    Travis setup: array declaration?

--- travis_automerge_to_jdi_light.sh
+++ travis_automerge_to_jdi_light.sh
@@ -9,7 +9,7 @@ JDI_LIGHT_GITHUB_REPO="jdi-testing/jdi-light"
 JDI_LIGHT_BRANCH="gh-pages"
 JDI_LIGHT_DIR="jdi-light"
 JDI_LIGHT_ANGULAR_DIR="angular"
-FILES_TO_EXCLUDE=(3rdpartylicenses.txt index.html)
+# FILES_TO_EXCLUDE=( "3rdpartylicenses.txt" "index.html" ) #todo this declaration fails
 SERVER_PORT=8001
 
 export GIT_COMMITTER_EMAIL='travis@travis'
@@ -17,9 +17,9 @@ export GIT_COMMITTER_NAME='Travis CI'
 
 # Start
 # Clone jdi-light and checkout the required branch
-repo_temp=$(mktemp -d)
-git clone "https://github.com/${JDI_LIGHT_GITHUB_REPO}" "$repo_temp"
-cd "$repo_temp"
+REPO_TEMP=$(mktemp -d)
+git clone "https://github.com/${JDI_LIGHT_GITHUB_REPO}" "${REPO_TEMP}"
+cd "${REPO_TEMP}"
 echo "git checkout gh-pages_____________"
 git checkout "$JDI_LIGHT_BRANCH"
 
@@ -28,17 +28,14 @@ cd ${TEST_SITE_DIR}/${ANGULAR_SITE_DIR}
 ([ ! -d "node_modules" ] && printf "node_modules dir is not found - performing npm install" && npm install)
 
 # Copy the required files
-rm -rf $repo_temp/${JDI_LIGHT_ANGULAR_DIR}/*
-cp -R ${TEST_SITE_DIR}/${ANGULAR_SITE_DIR}/dist/my-app/. $repo_temp/${JDI_LIGHT_ANGULAR_DIR}/
+rm -rf ${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/*
+cp -R ${TEST_SITE_DIR}/${ANGULAR_SITE_DIR}/dist/my-app/. ${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/
 
-# remove FILES_TO_EXCLUDE
-for f in ${FILES_TO_EXCLUDE[@]}
-do
-  rm ./${JDI_LIGHT_ANGULAR_DIR}/$f
-done
+rm ${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/3rdpartylicenses.txt index.html
+rm ${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/index.html
 
 # Add new files to git (or use git commit -a -m "commit_message")
-cd $repo_temp/${JDI_LIGHT_ANGULAR_DIR}
+cd ${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}
 git add --all
 # Print changes that are about to be committed
 git status
