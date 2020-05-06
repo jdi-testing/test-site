80f6c4ac5413ee587532e5c63f03c2794904101e
Dmitrii Shadrin <Dmitrii_Shadrin@externals.adidas.com>
2020-05-06 17:51:16 +0300

    Travis setup: Another attempt to conform with codacy standards

--- travis_automerge_to_jdi_light.sh
+++ travis_automerge_to_jdi_light.sh
@@ -77,7 +77,8 @@ PR_URL=$(hub pull-request --base "${JDI_LIGHT_BRANCH}" --head "${BRANCH_TO_MERGE
 
 printf "Following pull request has been created: ${PR_URL}\n"
 #PR_NUMBER=$(echo "${PR_URL}" | sed 's/[^0-9]*//g')
-PR_NUMBER="${PR_URL//[^0-9]/}"
+#PR_NUMBER="${PR_URL//[^0-9]/}"
+PR_NUMBER=$(echo "${MY_VAR}" | tr -d -c 0-9 )
 
 # Attempts to merge the request.
 # GITHUB_TOKEN must belong to user that has permission to push into BRANCH_TO_MERGE
