abfdd0c6fb3c267941fe787053036fb8cc476181
Dmitrii Shadrin <Dmitrii_Shadrin@externals.adidas.com>
2020-05-07 17:15:42 +0300

    Travis setup: Codestyle fix to meet Codacy expectations

--- travis_automerge_to_jdi_light.sh
+++ travis_automerge_to_jdi_light.sh
@@ -49,12 +49,12 @@ rm -rf "${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/3rdpartylicenses.txt"
 rm -rf "${REPO_TEMP}/${JDI_LIGHT_ANGULAR_DIR}/index.html"
 
 # Let's move to our jdi-light dir now
-cd ${REPO_TEMP}
+cd "${REPO_TEMP}"
 
 # Exclude angular-generated .js file from codacy review scope:
 ([ ! -f .codacy.yaml ] &&
   printf "\nCreating .codacy.yaml file\n" &&
-  printf "exclude_paths:\n  - '${JDI_LIGHT_ANGULAR_DIR}/**'\n" > ${REPO_TEMP}/.codacy.yaml &&
+  printf "exclude_paths:\n  - '${JDI_LIGHT_ANGULAR_DIR}/**'\n" > "${REPO_TEMP}/.codacy.yaml" &&
   git add .codacy.yaml)
 
 # Add all new files to git
