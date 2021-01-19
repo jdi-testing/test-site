6b07bc56f753aebe7a3c313818672e20b9928373
nadam131 <nickadam131@gmail.com>
2021-01-19 17:22:46 +0300

    change paths for .stories files in storybook settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+  stories: ["../src/**/*.stories.(ts|tsx|js|jsx|mdx)", "../stories/*.stories.(ts|tsx|js|jsx|mdx)"],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
