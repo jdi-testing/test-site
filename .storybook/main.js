60dad8edac40f7593bfc1c4f9556bd095a2445a4
nadam131 <nickadam131@gmail.com>
2021-01-19 16:06:49 +0300

    add @storybook/addon-docs for auto-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+};
