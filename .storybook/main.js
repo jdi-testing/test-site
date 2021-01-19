b97093023e6f05ffbdaa4df8d77565112b1dfc70
nadam131 <nickadam131@gmail.com>
2021-01-19 17:45:56 +0300

    change storybook glob pattern in paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
 module.exports = {
-  stories: ["../src/**/*.stories.(ts|tsx|js|jsx|mdx)", "../stories/*.stories.(ts|tsx|js|jsx|mdx)"],
+  stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)", "../stories/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    "@storybook/addon-essentials",
     {
       name: "@storybook/addon-docs",
       options: {
         configureJSX: true
       }
-    }
+    },
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
   ],
 };
