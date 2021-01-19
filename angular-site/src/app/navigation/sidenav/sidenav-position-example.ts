ea970a79c6d0855440e78bb2ca9d1078bccfec39
nadam131 <nickadam131@gmail.com>
2021-01-19 15:44:20 +0300

    clean repository from existing code

--- angular-site/src/app/navigation/sidenav/sidenav-position-example.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Implicit main content with two sidenavs */
-@Component({
-  selector: 'sidenav-position-example',
-  templateUrl: 'sidenav-position-example.html',
-  styleUrls: ['sidenav-position-example.css'],
-})
-export class SidenavPositionExample {}
