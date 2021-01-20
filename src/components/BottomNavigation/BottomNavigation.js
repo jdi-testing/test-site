49838a5878584d42814078a387d99a33060c6f86
nadam131 <nickadam131@gmail.com>
2021-01-20 12:55:13 +0300

    add BottomNavigation example of Material

---
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MBottomNavigation from '@material-ui/core/BottomNavigation';
+
+const BottomNavigation = (props) => <MBottomNavigation {...props} />;
+
+export default BottomNavigation;
---
+++ src/components/BottomNavigation/BottomNavigation.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import BottomNavigation from './BottomNavigation';
+
+export default {
+  title: 'Material Ui/Navs/BottomNavigation',
+  component: BottomNavigation,
+};
+
+const Template = (args) => (
+  <BottomNavigation {...args}>
+    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+  </BottomNavigation>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  showLabels: true,
+  value: 0,
+};
