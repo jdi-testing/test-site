609c1fdada3122e3882a1ce51d41e4036b5d2895
PETERSBURG\Ekaterina_Vasileva <ekaterina.vasileva@operative.com>
2021-01-25 17:29:27 +0300

    Date and time pickers components are added

---
+++ src/components/DateTimePicker/DateTimePicker.js
@@ -0,0 +1,57 @@
+// npm i --save date-fns@next @date-io/date-fns@1.3.13
+// npm install @material-ui/pickers
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+export default function MaterialUIPickers() {
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = (date) => {
+    console.log('Date is updated to ', date);
+    setSelectedDate(date);
+  };
+
+  const [selectedTime, setSelectedTime] = React.useState(new Date());
+
+  const handleTimeChange = (time) => {
+    console.log('Time is updated to ', time);
+    setSelectedTime(time);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+          value={selectedTime}
+          onChange={handleTimeChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+}
---
+++ src/components/DateTimePicker/DateTimePicker.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import MaterialUIPickers from './DateTimePicker';
+
+export default {
+  title: 'Material UI/Date Time Pickers',
+  component: MaterialUIPickers,
+};
+
+export const DateTimePickers = (args) => <MaterialUIPickers {...args}>MaterialUIPickers</MaterialUIPickers>;
