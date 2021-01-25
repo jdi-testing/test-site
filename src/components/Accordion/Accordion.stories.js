7cf86eb5e46acee3e3cfa51a0f451d0941e2b3eb
Stanislav Dmitriev <stanislav_dmitriev@epam.com>
2021-01-25 15:00:42 +0300

    Accordion elements implemented

---
+++ src/components/Accordion/Accordion.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Accordion from "./Accordion";
+import AccordionSummary from "./AccordionSummary";
+import AccordionDetails from "./AccordionDetails";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+export default {
+    title: 'Material UI/Surfaces/Accordion',
+    component: Accordion, AccordionSummary, AccordionDetails
+};
+
+const Template = (args) => (
+    <Accordion {...args}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content">Accordion</AccordionSummary>
+        <AccordionDetails>Accordion content</AccordionDetails>
+    </Accordion>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    showLabels: true,
+    value: 0,
+};
