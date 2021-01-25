4038610f6b1bf9063df75972596dadea5919fafd
shutkina87 <valentina.shutkina@icloud.com>
2021-01-25 23:31:00 +0300

    Grid component is added

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+
+export default function GridForm() {
+
+    return (<body>
+             <div style={{ padding: 20 }}>
+               <Grid container container direction="row" justify="center" alignItems="center">
+                 //Grid
+               </Grid>
+             </div>
+            </body>);
+}
---
+++ src/components/Grid/Grid.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import GridForm from './Grid';
+
+export default {
+    title: "Material Ui/Grid",
+    component: GridForm,
+}
+
+export const GridView = (args) => <GridForm {...args}>GridForm</GridForm>;
