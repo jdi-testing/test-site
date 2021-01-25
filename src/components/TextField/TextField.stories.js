78e7868932a44b5f9963a750fc472d0003328458
shutkina87 <valentina.shutkina@icloud.com>
2021-01-25 22:49:04 +0300

    TextField components are added

---
+++ src/components/TextField/TextField.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import TextFieldForm from './TextField';
+
+export default {
+    title: "Material Ui/TextField",
+    component: TextFieldForm,
+}
+
+export const TextFieldFormView = (args) => <TextFieldForm {...args}>TextFieldForm</TextFieldForm>;
