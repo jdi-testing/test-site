6faabed48e994ece9f1fa5d46352882a88a186e4
Fedor_Ermolin <fedor_ermolin@epam.com>
2021-01-25 17:36:20 +0300

    Added TextareaAutosize element

---
+++ src/components/TextareaAutosize/TextareaAutosize.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MTextareaAutosize from '@material-ui/core/TextareaAutosize';
+
+const TextareaAutosize = (props) => <MTextareaAutosize {...props} />;
+
+export default TextareaAutosize;
