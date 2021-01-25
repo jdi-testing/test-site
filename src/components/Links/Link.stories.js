47e6d153f2ed533ff6cbabee12b10658d22212c3
alpakhomov <4778468+alpakhomov@users.noreply.github.com>
2021-01-25 13:45:09 +0300

    Added material-ui Links component

---
+++ src/components/Links/Link.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Link from './Link';
+
+export default {
+  title: 'Material Ui/Navs/Link',
+  component: Link,
+};
+
+const Template = (args) => (
+  <Link {...args} onClick={() => console.log('link clicked')}>
+    Link
+  </Link>
+);
+
+export const Default = Template.bind({});
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  color: 'secondary',
+};
+
+export const TextPrimary = Template.bind({});
+TextPrimary.args = {
+  color: 'textPrimary',
+};
+
+export const TextSecondary = Template.bind({});
+TextSecondary.args = {
+  color: 'textSecondary',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  color: 'error',
+};
+
+export const UnderlineAlways = Template.bind({});
+UnderlineAlways.args = {
+  underline: 'always',
+};
