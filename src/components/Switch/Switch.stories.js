fefc40bd8497a2254932b10e32bb5209862079f7
Valentin_Bodrov <befearless4428876@gmail.com>
2021-01-25 15:02:41 +0300

    Switch component added

---
+++ src/components/Switch/Switch.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Switch from './Switch';
+
+export default {
+    title: 'Material Ui/Switch',
+    component: Switch
+};
+
+const Template = (args) => <Switch {...args} onClick={() => console.log('Switch pressed')}/>;
+
+// Stories
+export const Default = Template.bind({});
+Default.args = {
+    color: 'default'
+}
+
+export const Primary = Template.bind({});
+Primary.args = {
+    variant: 'contained',
+    color: 'primary'
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    variant: 'contained',
+    color: 'secondary'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    disabled: true
+};
+
+export const DisabledAndChecked = Template.bind({});
+DisabledAndChecked.args = {
+    checked: true,
+    disabled: true
+};
+
