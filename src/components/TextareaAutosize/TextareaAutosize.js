e09d655ff578d3bad2afcd68a348000f2d566716
Fedor_Ermolin <fedor_ermolin@epam.com>
2021-01-26 17:19:12 +0300

    Added Portal element

--- src/components/TextareaAutosize/TextareaAutosize.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MTextareaAutosize from '@material-ui/core/TextareaAutosize';
-
-const TextareaAutosize = (props) => <MTextareaAutosize {...props} />;
-
-export default TextareaAutosize;
