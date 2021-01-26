18231d9c4abf0baf1773af5db19e7e96948c9feb
Alla Blinkova <alla_blinkova@epam.com>
2021-01-26 15:47:28 +0300

    2617: added grid list components

---
+++ src/components/GridList/ImageGridList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { tileData } from './data/TileData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+}));
+
+export default function ImageGridList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={160} className={classes.gridList} cols={2}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img} cols={tile.cols || 1}>
+            <img src={tile.img} alt={tile.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
---
+++ src/components/GridList/data/TileData.js
@@ -0,0 +1,33 @@
+import morning from './morning.jpg';
+import plant from './plant.jpg';
+import hats from './hats.jpg';
+import bike from './bike.jpg';
+import camera from './camera.jpg';
+
+export const tileData = [
+  {
+    img: morning,
+    title: 'morning',
+    author: 'author',
+  },
+  {
+    img: plant,
+    title: 'plant',
+    author: 'author2',
+  },
+  {
+    img: hats,
+    title: 'hats',
+    author: 'author3',
+  },
+  {
+    img: bike,
+    title: 'bike',
+    author: 'author4',
+  },
+  {
+    img: camera,
+    title: 'camera',
+    author: 'author5',
+  },
+];
