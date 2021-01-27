cb46e75b4feac462d481ed2c7ddac7381fb96717
PETERSBURG\Ekaterina_Vasileva <ekaterina.vasileva@operative.com>
2021-01-27 13:25:55 +0300

    Added disabled, range and vertical sliders

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import MSlider from '@material-ui/core/Slider';
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
-const Slider = (props) => <MStyledSlider {...props} />;
-const MStyledSlider = styled(MSlider)`
-  width: 100px;
-`;
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
 
-export default Slider;
+export default function ContinuousSlider() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography id="continuous-slider" gutterBottom>
+        Default Slider
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+        </Grid>
+      </Grid>
+      <Typography id="disabled-slider" gutterBottom>
+        Disabled slider
+      </Typography>
+      <Slider disabled defaultValue={30} aria-labelledby="disabled-slider" />
+    </div>
+  );
+}
--- src/components/Slider/Slider.stories.js
+++ src/components/Slider/Slider.stories.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import Slider from './Slider';
+import RangeSlider from './RangeSlider';
+import VerticalSlider from './VerticalSlider';
 
 export default {
-  title: 'Material Ui/Slider',
+  title: 'Material Ui/Sliders',
   component: Slider,
 };
 
-const Template = (args) => <Slider {...args} onClick={() => console.log('123')}>Slider</Slider>;
-
 // Stories
-export const Default = Template.bind({});
+export const DefaultAndDisabledSliders = (args) => <Slider {...args}>Slider</Slider>;
+
+export const Range_Slider = (args) => <RangeSlider {...args}>RangeSlider</RangeSlider>;
+
+export const Vertical_Slider = (args) => <VerticalSlider {...args}>VerticalSlider</VerticalSlider>;
