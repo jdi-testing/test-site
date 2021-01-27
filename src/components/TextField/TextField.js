079ba052f491368efd867055cf184caa7a50c9f2
shutkina87 <valentina.shutkina@icloud.com>
2021-01-27 16:57:21 +0300

    TextField properties are set from stories.js

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
-export default function TextFieldForm() {
+export default function BuildTextField(props) {
     const inputProps = {
       step: 300,
     };
 
-    return (<Grid container justify="space-around" alignItems="baseline">
-    <TextField id="standard-basic" label="First" type="text" required="true" autoFocus="true" inputProps={inputProps} />
-    <TextField id="second-basic" label="Second with default" defaultValue="Default value" inputProps={inputProps} />
-    <TextField id="disabled-field" label="Disabled field" disabled="true" inputProps={inputProps} />
-    <TextField id="filled-basic" label="Filled" variant="filled" />
-    <TextField id="number" type="number" label="Number" variant="standard" />
-    <TextField id="error" label="Error" error="true" helperText="Incorrect value" variant="filled" />
-    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-    <TextField id="outlined-basic-text" label="Outlined with Helper text" helperText="Please fill text in this field" variant="outlined" />
-    <TextField id="time-field" type="time" helperText="Please fill time" inputProps={inputProps} />
-    <TextField id="email" type="email" helperText="Email field" inputProps={inputProps} />
-    </Grid>);
+    return (
+    <TextField {...props} inputProps={inputProps} />
+    );
 }
