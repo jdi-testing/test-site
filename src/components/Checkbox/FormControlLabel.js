d8b824f2f83462b355b32012e171a4794787af20
shutkina87 <valentina.shutkina@icloud.com>
2021-01-28 00:49:06 +0300

    Checkbox components are added

---
+++ src/components/Checkbox/FormControlLabel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const FormControlLabelConst = (props) => <FormControlLabel {...props} />;
+
+export default FormControlLabelConst;
---
+++ src/components/Checkbox/FormGroup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import FormGroup from '@material-ui/core/FormGroup';
+
+const FormGroupConst = (props) => <FormGroup {...props} />;
+
+export default FormGroupConst;
