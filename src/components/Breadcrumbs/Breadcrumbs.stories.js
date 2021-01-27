db5aa1bbdf459aa58d94e9eadc0e406182ad9bc9
Andrei0 <andru.martynov@gmail.com>
2021-01-27 13:44:58 +0300

    Refactored in accordance with codacy

--- src/components/Breadcrumbs/Breadcrumbs.stories.js
+++ src/components/Breadcrumbs/Breadcrumbs.stories.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Breadcrumbs from "./Breadcrumbs";
-import RouterBreadcrumbs from "./RouterBreadcrumbs";
-import BreadcrumbsWithIcons from "./BreadcrumbsWithIcons";
-import CustomizedBreadcrumbs from "./CustomizedBreadcrumbs";
-import CustomSeparatorsBreadcrumbs from "./CustomSeparatorsBreadcrumbs";
-import SimpleBreadcrumbs from "./SimpleBreadcrumbs";
-import CollapsedBreadcrumbs from "./CollapsedBreadcrumbs";
+import Breadcrumbs from './Breadcrumbs';
+import RouterBreadcrumbs from './RouterBreadcrumbs';
+import BreadcrumbsWithIcons from './BreadcrumbsWithIcons';
+import CustomizedBreadcrumbs from './CustomizedBreadcrumbs';
+import CustomSeparatorsBreadcrumbs from './CustomSeparatorsBreadcrumbs';
+import SimpleBreadcrumbs from './SimpleBreadcrumbs';
+import CollapsedBreadcrumbs from './CollapsedBreadcrumbs';
 
 export default {
-    title: 'Material Ui/Navs/Breadcrumbs',
-    component: Breadcrumbs,
+  title: 'Material Ui/Navs/Breadcrumbs',
+  component: Breadcrumbs,
 };
 
 export const Simple = SimpleBreadcrumbs.bind({});
@@ -23,8 +22,3 @@ export const Collapsed = CollapsedBreadcrumbs.bind({});
 export const RouterIntegration = RouterBreadcrumbs.bind({});
 
 export const Customized = CustomizedBreadcrumbs.bind({});
-
-
-
-
-
--- src/components/Breadcrumbs/CustomSeparatorsBreadcrumbs.js
+++ src/components/Breadcrumbs/CustomSeparatorsBreadcrumbs.js
@@ -6,50 +6,50 @@ import Link from '@material-ui/core/Link';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
+  root: {
+    '& > * + *': {
+      marginTop: theme.spacing(2),
     },
+  },
 }));
 
 function handleClick(event) {
-    event.preventDefault();
-    console.info('You clicked a breadcrumb.');
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
 }
 
 export default function CustomSeparator() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Breadcrumbs separator="›" aria-label="breadcrumb">
-                <Link color="inherit" href="/" onClick={handleClick}>
-                    Material-UI
-                </Link>
-                <Link color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-                    Core
-                </Link>
-                <Typography color="textPrimary">Breadcrumb</Typography>
-            </Breadcrumbs>
-            <Breadcrumbs separator="-" aria-label="breadcrumb">
-                <Link color="inherit" href="/" onClick={handleClick}>
-                    Material-UI
-                </Link>
-                <Link color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-                    Core
-                </Link>
-                <Typography color="textPrimary">Breadcrumb</Typography>
-            </Breadcrumbs>
-            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-                <Link color="inherit" href="/" onClick={handleClick}>
-                    Material-UI
-                </Link>
-                <Link color="inherit" href="/getting-started/installation/" onClick={handleClick}>
-                    Core
-                </Link>
-                <Typography color="textPrimary">Breadcrumb</Typography>
-            </Breadcrumbs>
-        </div>
-    );
+  return (
+    <div className={ classes.root }>
+      <Breadcrumbs separator="›" aria-label="breadcrumb">
+        <Link color="inherit" href="/" onClick={ handleClick }>
+          Material-UI
+        </Link>
+        <Link color="inherit" href="/getting-started/installation/" onClick={ handleClick }>
+          Core
+        </Link>
+        <Typography color="textPrimary">Breadcrumb</Typography>
+      </Breadcrumbs>
+      <Breadcrumbs separator="-" aria-label="breadcrumb">
+        <Link color="inherit" href="/" onClick={ handleClick }>
+          Material-UI
+        </Link>
+        <Link color="inherit" href="/getting-started/installation/" onClick={ handleClick }>
+          Core
+        </Link>
+        <Typography color="textPrimary">Breadcrumb</Typography>
+      </Breadcrumbs>
+      <Breadcrumbs separator={ <NavigateNextIcon fontSize="small"/> } aria-label="breadcrumb">
+        <Link color="inherit" href="/" onClick={ handleClick }>
+          Material-UI
+        </Link>
+        <Link color="inherit" href="/getting-started/installation/" onClick={ handleClick }>
+          Core
+        </Link>
+        <Typography color="textPrimary">Breadcrumb</Typography>
+      </Breadcrumbs>
+    </div>
+  );
 }
