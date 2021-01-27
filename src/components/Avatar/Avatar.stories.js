e7eff432d118d91d463c47c525edd645bc8c74b6
alpakhomov <4778468+alpakhomov@users.noreply.github.com>
2021-01-27 17:18:56 +0300

    Added material-ui Avatar component

---
+++ src/components/Avatar/Avatar.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+import Avatar from './Avatar';
+import SampleAvatar from './assets/sample_avatar.jpg';
+
+export default {
+  title: 'Material Ui/Data Display/Avatar',
+  component: Avatar,
+};
+
+export const NoImage = Avatar.bind({});
+
+export const Circle = Avatar.bind({});
+Circle.args = {
+  src: SampleAvatar,
+};
+
+export const Square = Avatar.bind({});
+Square.args = {
+  src: SampleAvatar,
+  variant: 'square',
+};
+
+export const Rounded = Avatar.bind({});
+Rounded.args = {
+  src: SampleAvatar,
+  variant: 'rounded',
+};
+
+export const FallbackAlt = Avatar.bind({});
+FallbackAlt.args = {
+  src: 'broken_image',
+  alt: 'Lorem',
+};
+
+const LetterTemplate = () => <Avatar>A</Avatar>;
+export const Letter = LetterTemplate.bind({});
+
+const IconTemplate = () => (
+  <Avatar>
+    <AssignmentIcon />
+  </Avatar>
+);
+export const Icon = IconTemplate.bind({});
