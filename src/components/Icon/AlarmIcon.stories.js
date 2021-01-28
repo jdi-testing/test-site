bc1b9b3865a708272cfa6b193e8729b8d8f841d6
AlinaSelyutina <a.v.blazhko97@gmail.com>
2021-01-28 12:18:14 +0300

    2652: Added Data Display - Material Icons

---
+++ src/components/Icon/AlarmIcon.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import AlarmIcon from './AlarmIcon';
+
+export default {
+    title: 'Material Ui/Data Display/Icon',
+    component: AlarmIcon,
+};
+
+const Template = (args) => (
+    <AlarmIcon {...args}>
+        Link
+    </AlarmIcon>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+    color:"default",
+    fontSize:"default"
+}
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    color:"secondary",
+    fontSize:"large"
+}
