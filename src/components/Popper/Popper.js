9c9b430dafead1837024a15ecc09da5e48429d72
Fedor_Ermolin <fedor_ermolin@epam.com>
2021-01-29 10:21:32 +0300

    Added Popper element

---
+++ src/components/Popper/Popper.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MPopper from '@material-ui/core/Popper';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Popper() {
+return <div>
+            <button aria-describedby='popperButton' type="button"
+                onClick={() => console.log('Toggle Popper clicked')}>
+                Toggle Popper
+            </button>
+            <MPopper id = 'simple-popper' anchorEl='popperButton' open= 'true'>
+              <div>The content of the Popper.</div>
+              </MPopper>
+      </div>;
+}
+
+
+export default Popper;
