a7911c57b56fa34105d5fdea2db3fe0a76f4b67d
shutkina87 <valentina.shutkina@icloud.com>
2021-02-01 20:02:07 +0300

    AppBar component is added

--- src/components/AppBar/AppBar.stories.js
+++ src/components/AppBar/AppBar.stories.js
@@ -3,11 +3,12 @@ import React from 'react';
 import BuildAppBar from './AppBar';
 
 export default {
-    title: "Material Ui/AppBar",
+    title: "Material Ui/Surfaces/AppBar",
     component: BuildAppBar,
 }
 
 export const SimpleAppBar = (args) => <BuildAppBar {...args}>BuildAppBar</BuildAppBar>;
 SimpleAppBar.args = {
                       position: "static",
+                      color: "primary",
                   };
