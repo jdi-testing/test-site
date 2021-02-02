1db15eb7fbd314d5da3ba207d5abe876b4aefbe0
Fedor_Ermolin <fedor_ermolin@epam.com>
2021-02-02 18:05:15 +0300

    Added Popper element

--- src/components/Popper/PositionedPopper.js
+++ src/components/Popper/PositionedPopper.js
@@ -9,7 +9,8 @@ import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: 500,
+    hight: 800,
+    width: 600,
   },
   typography: {
     padding: theme.spacing(2),
@@ -29,8 +30,8 @@ export default function PositionedPopper() {
   };
 
   return (
-    <div className={classes.root}>
-      <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+    <div className={classes.root} style={{position: 'absolute', top: '20%'}}>
+      <Popper open={open} anchorEl={anchorEl}  placement={placement} transition>
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
             <Paper>
@@ -39,38 +40,21 @@ export default function PositionedPopper() {
           </Fade>
         )}
       </Popper>
+
       <Grid container justify="center">
-        <Grid item>
-          <Button onClick={handleClick('top-start')}>top-start</Button>
-          <Button onClick={handleClick('top')}>top</Button>
-          <Button onClick={handleClick('top-end')}>top-end</Button>
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item xs={6}>
-          <Button onClick={handleClick('left-start')}>left-start</Button>
-          <br />
-          <Button onClick={handleClick('left')}>left</Button>
-          <br />
-          <Button onClick={handleClick('left-end')}>left-end</Button>
-        </Grid>
-        <Grid item container xs={6} alignItems="flex-end" direction="column">
+        <Grid item container xs={6} alignItems="center" direction="column">
           <Grid item>
-            <Button onClick={handleClick('right-start')}>right-start</Button>
+             <Button onClick={handleClick('top')}>top</Button>
+          </Grid>
+          <Grid item>
+            <Button onClick={handleClick('left')}>left</Button>
           </Grid>
           <Grid item>
             <Button onClick={handleClick('right')}>right</Button>
           </Grid>
           <Grid item>
-            <Button onClick={handleClick('right-end')}>right-end</Button>
+            <Button onClick={handleClick('bottom')}>bottom</Button>
           </Grid>
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item>
-          <Button onClick={handleClick('bottom-start')}>bottom-start</Button>
-          <Button onClick={handleClick('bottom')}>bottom</Button>
-          <Button onClick={handleClick('bottom-end')}>bottom-end</Button>
         </Grid>
       </Grid>
     </div>
