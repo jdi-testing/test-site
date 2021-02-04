10901993f0e303f9f0ac85d5c4ca51513a3e92c3
AlinaSelyutina <a.v.blazhko97@gmail.com>
2021-02-04 16:50:43 +0300

    small fix

--- src/components/Divider/Divider.stories.js
+++ src/components/Divider/Divider.stories.js
@@ -1,11 +1,11 @@
-import Dividers from "./InsetDividers";
+import InsetDividers from "./InsetDividers";
 import VerticalDividers from "./VerticalDividers";
 import React from "react";
 
 export default {
-    title: 'Material Ui/Data Display/InsetDividers',
-    component: Dividers, VerticalDividers
+    title: 'Material Ui/Data Display/Dividers',
+    component: InsetDividers, VerticalDividers
 };
 
-export const InsetDividersView = (args) => <Dividers {...args}>Types View</Dividers>;
+export const InsetDividersView = (args) => <InsetDividers {...args}>Types View</InsetDividers>;
 export const VerticalDividersView = (args) => <VerticalDividers {...args}>Types View</VerticalDividers>;
