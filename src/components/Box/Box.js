5deb1ce4d849d01a394529cfaf25800f5d0b1feb
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-03-22 20:00:34 +0300

    Boxes are added

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import MBox from '@material-ui/core/Box';
+import React, {useState} from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
-const Box = (props) => <MBox {...props} />;
-
-export default Box;
+export default function Boxes() {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <h1>Boxes</h1>
+      <Box component="span" onClick={() => console.log('The main box clicked')}>
+        <Box m={1} onClick={() => setText('First button')}>
+          <Button variant='contained' color='primary'>First button</Button>
+        </Box>
+        <Box m={1} onClick={() => setText('Second button')}>
+          <Button variant='outlined' color='secondary' >Second button</Button>
+        </Box>
+        <Box m={1} onClick={() => setText('Third button')}>
+          <Button disabled={true}>Third button</Button>
+        </Box>
+      </Box>
+      <p id="lastClickContent">You clicked {text}</p>
+    </div>
+  );
+}
