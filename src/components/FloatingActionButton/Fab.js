ff92f2cdee89ea156c9efba857369a449d5c535a
Natalia_Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-03-25 22:38:45 +0300

    +Floating Action Buttons

--- src/components/FloatingActionButton/Fab.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NFab from '@material-ui/core/Fab';
-
-const Fab = (props) => <NFab {...props} />;
-
-export default Fab;
--- src/components/FloatingActionButton/FloatingActionButton.js
+++ src/components/FloatingActionButton/FloatingActionButton.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import NavigationIcon from '@material-ui/icons/Navigation';
+import FloatingActionButtonZoom from "./AnimationFloating";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,22 +20,31 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FloatingActionButtons() {
   const classes = useStyles();
+  const [text, setText] = useState("");
 
   return (
-    <div className={classes.root}>
-      <Fab color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
-      <Fab color="secondary" aria-label="edit">
-        <EditIcon />
-      </Fab>
-      <Fab variant="extended">
-        <NavigationIcon className={classes.extendedIcon} />
-        Navigate
-      </Fab>
-      <Fab disabled aria-label="like">
-        <FavoriteIcon />
-      </Fab>
+    <div>
+      <h1>Floating Action Buttons</h1>
+      <h2>Basic buttons</h2>
+      <div className={classes.root} id={"basicActionBtns"}>
+        <Fab color="primary" aria-label="add" onClick={() => setText("Add")}>
+          <AddIcon />
+        </Fab>
+        <Fab color="secondary" aria-label="edit" onClick={() => setText("Edit")}>
+          <EditIcon />
+        </Fab>
+        <Fab variant="extended" onClick={() => setText("Navigate")}>
+          <NavigationIcon className={classes.extendedIcon} />
+          Navigate
+        </Fab>
+        <Fab disabled aria-label="like" onClick={() => setText("Like")}>
+          <FavoriteIcon />
+        </Fab>
+      </div>
+      <p id={"basicActionBtnsLastClick"}>Last click: {text}</p>
+
+      <h2>Animated buttons</h2>
+      <FloatingActionButtonZoom id={"animatedActionBtns"} />
     </div>
   );
 }
