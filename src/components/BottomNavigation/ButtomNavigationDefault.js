f786a3bcc805852ffcd5dd1f3d759e6feff39010
Natalia_Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2021-03-27 22:44:41 +0300

    fix initial

--- src/components/BottomNavigation/ButtomNavigationDefault.js
+++ src/components/BottomNavigation/ButtomNavigationDefault.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles({
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-  const [text, setText] = React.useState(0);
+  const [text, setText] = React.useState("");
 
   return (
     <div>
