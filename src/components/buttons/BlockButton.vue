6770b1286c3abaa4d47cd11ae96a54c7f27a1acf
Rustam Saitov <78560640+strrust@users.noreply.github.com>
2021-08-09 10:01:32 +0300

    Added buttons and floating action buttons

---
+++ src/components/buttons/BlockButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h2>Block Button</h2>
+    <v-btn 
+      block
+      @click="click"
+    >
+      Block Button
+    </v-btn>
+    <v-text-field id= "block-button-state" textarea v-model="state" readonly></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    state: '',
+  }),
+
+  methods: {
+    async click() {
+      this.state = 'Block button clicked';
+    },
+  },
+};
+</script>
---
+++ src/components/buttons/TileButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <h2>Tile Button</h2>
+    <div class="text-center">
+    <v-btn
+      tile
+      color="success"
+      @click="click"
+    >
+      <v-icon left>
+        mdi-pencil
+      </v-icon>
+      Edit
+    </v-btn>
+  </div>
+  <v-text-field id="tile-button-state" textarea v-model="state" readonly></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({ 
+    state: '', 
+  }),
+
+  methods: {
+    click() {
+      this.state = 'Tile button clicked';
+    },
+  },
+};
+</script>
