d49e4e7cb60936ef1b16008fee8366456cc181eb
betapl3b <38052075+betapl3b@users.noreply.github.com>
2021-08-16 11:14:27 +0300

    Added Selects page (#179)
    Co-authored-by: Albert_Safin <Albert_Safin@epam.com>

--- src/router/index.js
+++ src/router/index.js
@@ -155,6 +155,11 @@ const routes = [
     component: () => import('../views/Overlays.vue'),
   },
   {
+    path: '/selects',
+    name: 'Selects',
+    component: () => import('../views/Selects.vue'),
+  },
+  {
     path: '/snackbars',
     name: 'Snackbars',
     component: () => import('../views/Snackbars.vue'),
