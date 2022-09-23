81803e7e975e6fbcb7ea8ea60c57fa0c56c30330
omega790 <prikhodko.olena@gmail.com>
2022-09-23 19:58:18 +0400

    4342 sheets add outlined, shaped, tile

---
+++ components/sheets/OutlinedSheet.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <v-sheet
+      color="grey lighten-3"
+      height="100"
+      width="100"
+      outlined
+      >
+    </v-sheet>
+  </v-container>
+</template>
