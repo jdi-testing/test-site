292acc20b9cec7dcd86d11dcd66f65106e9d207b
Maiia_Afanaseva <Maiia_Afanaseva@epam.com>
2022-10-20 22:05:59 +0300

    Task4359_addNewFeaturesCheckBoxes_Fixed

--- components/form_inputs/checkboxes/VariousAttributesCheckbox.vue
+++ components/form_inputs/checkboxes/VariousAttributesCheckbox.vue
@@ -1,48 +1,46 @@
 <template>
   <v-container fluid>
    <v-row align="center">
-     <span class="mr-3">Success, Readonly</span>
-         <p>{{ selected }}</p>
+    <v-col>
+     <span id="SuccessReadonlyCheckbox" class="mr-3">Success, Readonly</span>
              <v-checkbox
-               id="SuccessReadonlyCheckbox"
                v-model="selected"
                label="John"
                value="John"
                success
+               readonly
              ></v-checkbox>
              <v-checkbox
-               id="SuccessReadonlyCheckbox"
                v-model="selected"
                label="Jacob"
                value="Jacob"
                success
+               readonly
              ></v-checkbox>
+    </v-col>
    </v-row>
    <v-row align="center">
-        <span class="mr-3">Error, Hint, Back-ground color, Dense</span>
-             <p>{{ selected }}</p>
-                 <v-checkbox
-                   id="ErrorDenseCheckboxWithHint"
-                   v-model="selected"
-                   label="John"
-                   value="John"
-                   error
-                   hint="some hint"
-                   persistent-hint
-                   background-color="light-blue"
-                   dense
-                 ></v-checkbox>
-                 <v-checkbox
-                   id="ErrorDenseCheckboxWithHint"
-                   v-model="selected"
-                   label="Jacob"
-                   value="Jacob"
-                   error
-                   hint="another some hint"
-                   persistent-hint
-                   background-color="orange"
-                   dense
-                 ></v-checkbox>
+     <span id="ErrorDenseCheckboxWithHint" class="mr-3">Error, Hint, Back-ground color, Dense</span>
+             <v-checkbox
+               v-model="selected"
+               label="John"
+               value="John"
+               error
+               hint="some hint"
+               persistent-hint
+               background-color="light-blue"
+               dense
+             ></v-checkbox>
+             <v-checkbox
+               v-model="selected"
+               label="Jacob"
+               value="Jacob"
+               error
+               hint="another some hint"
+               persistent-hint
+               background-color="orange"
+               dense
+             ></v-checkbox>
       </v-row>
   </v-container>
 </template>
