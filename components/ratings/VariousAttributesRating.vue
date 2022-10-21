6824a85b771af8ff29056cc7003238034fb734c5
Maiia_Afanaseva <Maiia_Afanaseva@epam.com>
2022-10-21 17:21:56 +0300

    Task4344_addNewFeaturesRatings

---
+++ components/ratings/VariousAttributesRating.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid>
+   <v-row align="center">
+     <span id="ClearableRating" class="mr-3">Clearable</span>
+          <v-rating
+                v-model="rating"
+                color="red lighten-3"
+                background-color="grey lighten-1"
+                large
+                clearable
+              ></v-rating>
+   </v-row>
+   <v-row align="center">
+     <span id="OpenCloseDelayRating" class="mr-3">Open-delay, close-delay</span>
+          <v-rating
+                v-model="rating1"
+                color="orange"
+                background-color="blue lighten-3"
+                large
+                hover
+                open-delay="1000"
+                close-delay="1000"
+          ></v-rating>
+   </v-row>
+   <v-row align="center">
+        <span id="RatingWithValue" class="mr-3">Value</span>
+             <v-rating
+                   v-model="rating2"
+                   length="7"
+                   size="64"
+                   value="5"
+             ></v-rating>
+      </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    rating: 2,
+  }),
+};
+</script>
