9caa04bd40a1e663dd6ce85c080c42f4d3272bba
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-10-27 11:59:38 +0300

    1

---
+++ components/icons/DisabledIcons.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-row justify="space-around">
+        <v-icon
+          large
+          color="green darken-2"
+          disabled
+        >
+          mdi-domain
+        </v-icon>
+
+        <v-icon aria-label="My Account" role="img""
+        disabled>
+          mdiAccount
+        </v-icon>
+
+        <script>
+        import { mdiAccount } from "@mdi/js";
+
+        export default {
+          data: () => ({
+            icons: {
+              mdiAccount
+            }
+          })
+        };
+        </script>
+    </v-row>
+</template>
---
+++ components/icons/SemanticIcons.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-row justify="space-around">
+        <v-icon aria-hidden="false">
+          mdi-account
+        </v-icon>
+
+        <v-icon aria-label="My Account" role="img" aria-hidden="false">
+          mdiAccount
+        </v-icon>
+
+        <script>
+        import { mdiAccount } from "@mdi/js";
+
+        export default {
+          data: () => ({
+            icons: {
+              mdiAccount
+            }
+          })
+        };
+        </script>
+    </v-row>
+</template>
