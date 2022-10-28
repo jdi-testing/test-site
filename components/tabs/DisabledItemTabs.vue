1a979e25497ba6b714f569b06c76bda498b771fd
Georgy Pankratenko <georgy_pankratenko@epam.com>
2022-10-28 15:21:55 +0200

    4241 Update Tabs

---
+++ components/tabs/DisabledItemTabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-tabs
+      v-model="tab"
+    >
+      <v-tab
+        v-for="tab in tabs"
+        :key="tab.item"
+      >
+        {{ tab.item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items
+      v-model="tab"
+    >
+      <v-tab-item
+        v-for="tab in tabs"
+        :key="tab.item"
+        :disabled="tab.disabled"
+      >
+        <v-card flat>
+          <v-card-text v-text="'Content'"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        tabs: [
+          {item: 'One', disabled: false},
+          {item: 'Two', disabled: true},
+          {item: 'Three', disabled: false},
+        ],
+      }
+    },
+  }
+</script>
