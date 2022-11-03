d20d78ef7ef24e2556ede4fd96fe0e0917e67058
Liliia Iskanderova <liliia.iskanderova@ukg.com>
2022-11-03 13:16:14 +0100

    4439 Update listitemgroups2

--- components/groups/list_item_groups/WithSubGroupListItemGroup.vue
+++ components/groups/list_item_groups/WithSubGroupListItemGroup.vue
@@ -1,64 +1,89 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="400"
+    width="300"
   >
-    <v-list flat>
-      <v-list-group
-        v-model="model"
-        color="indigo"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+      <v-list-group
+        :value="true"
+        prepend-icon="mdi-account-circle"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>Users</v-list-item-title>
+        </template>
+
         <v-list-group
-          v-model="model"
+          :value="true"
+          no-action
           sub-group
         >
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-          >
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-
+          <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
+              <v-list-item-title>Admin</v-list-item-title>
             </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in admins"
+            :key="i"
+            link
+          >
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group
+          no-action
+          sub-group
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Actions</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="([title, icon], i) in cruds"
+            :key="i"
+            link
+          >
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
           </v-list-item>
         </v-list-group>
       </v-list-group>
     </v-list>
   </v-card>
 </template>
+
 <script>
-export default {
-  data: () => ({
-    items: [
-      {
-        icon: 'mdi-wifi',
-        text: 'Wifi',
-      },
-      {
-        icon: 'mdi-bluetooth',
-        text: 'Bluetooth',
-      },
-      {
-        icon: 'mdi-chart-donut',
-        text: 'Data Usage',
-      },
-    ],
-    model: 1,
-  }),
-};
+  export default {
+    data: () => ({
+      admins: [
+        ['Management', 'mdi-account-multiple-outline'],
+        ['Settings', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
+      ],
+    }),
+  }
 </script>
