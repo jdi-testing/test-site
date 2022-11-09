7e8e26678dce264dfd5cfbd8b22e83a1a6efe751
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-11-09 11:34:35 +0300

    errors ready

--- components/form_inputs/range_sliders/DifferentRangerSliders.vue
+++ components/form_inputs/range_sliders/DifferentRangerSliders.vue
@@ -9,30 +9,46 @@
       :hide-details="isDetailsHidden"
       :error="isError"
       :background-color="backgroundColor"
+      :color="color"
+      :track-color="trackColor"
     ></v-range-slider>
 
     <v-col>
       <p class="text-h5">Settings</p>
 
+      <v-subheader>Dark theme</v-subheader>
       <v-row class="mx-5 align-center">
-        <span>Dark theme</span>
         <v-switch v-model="isDarkTheme" class="ma-0 ml-5" />
       </v-row>
 
+      <v-subheader>Hide details</v-subheader>
       <v-row class="mx-5 align-center">
-        <span>Hide details</span>
         <v-switch v-model="isDetailsHidden" class="ma-0 ml-5" />
       </v-row>
 
+      <v-subheader>Error</v-subheader>
       <v-row class="mx-5 align-center">
-        <span>Error</span>
         <v-switch v-model="isError" class="ma-0 ml-5" />
       </v-row>
 
+      <v-subheader>Background color</v-subheader>
       <v-row class="mx-5 align-center">
-        <span>Background color</span>
         <v-color-picker
           v-model="backgroundColor"
+        ></v-color-picker>
+      </v-row>
+
+      <v-subheader>Color</v-subheader>
+      <v-row class="mx-5 align-center">
+        <v-color-picker
+          v-model="color"
+        ></v-color-picker>
+      </v-row>
+
+      <v-subheader>Track-color</v-subheader>
+      <v-row class="mx-5 align-center">
+        <v-color-picker
+          v-model="trackColor"
         ></v-color-picker>
       </v-row>
     </v-col>
@@ -47,7 +63,9 @@ export default {
     isDarkTheme: false,
     isDetailsHidden: false,
     isError: false,
-    backgroundColor: "green",
+    backgroundColor: "Blue",
+    color: "Orange",
+    trackColor: "Green",
   }),
 };
 </script>
