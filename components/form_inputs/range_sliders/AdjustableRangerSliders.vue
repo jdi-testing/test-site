3782b249d74af8732cafb4d0e7f1dbcbefda3bd3
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-11-09 15:56:33 +0300

    pretty

--- components/form_inputs/range_sliders/AdjustableRangerSliders.vue
+++ components/form_inputs/range_sliders/AdjustableRangerSliders.vue
@@ -3,37 +3,37 @@
 		<v-range-slider :dark="isDarkTheme" min="-50" max="50" :value="[-25, 25]" hint="Some hint" :hide-details="isDetailsHidden" :error="isError" :background-color="backgroundColor" :color="color" :track-color="trackColor" :track-fill-color="trackFillColor" :thumb-color="thumbColor"/>
 		<p class="text-h5">Settings</p>
 		<v-row class="justify-space-around">
-			<v-card class="d-flex ma-2">
+			<v-card class="d-flex ma-5">
 				<v-subheader>Dark theme</v-subheader>
 				<v-switch v-model="isDarkTheme" class="ma-0 ml-5"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card class="d-flex ma-5">
 				<v-subheader>Hide details</v-subheader>
 				<v-switch v-model="isDetailsHidden" class="ma-0 ml-5"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card class="d-flex ma-5">
 				<v-subheader>Error</v-subheader>
 				<v-switch v-model="isError" class="ma-0 ml-5"/>
 			</v-card>
 		</v-row>
 		<v-row class="justify-space-around">
-			<v-card class="d-flex ma-2">
+			<v-card d-flex ma-5>
 				<v-subheader>Background color</v-subheader>
 				<v-color-picker v-model="backgroundColor"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card d-flex ma-5>
 				<v-subheader>Color</v-subheader>
 				<v-color-picker v-model="color"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card d-flex ma-5>
 				<v-subheader>Track-color</v-subheader>
 				<v-color-picker v-model="trackColor"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card d-flex ma-5>
 				<v-subheader>Track-fill-color</v-subheader>
 				<v-color-picker v-model="trackFillColor"/>
 			</v-card>
-			<v-card class="d-flex ma-2">
+			<v-card d-flex ma-5>
 				<v-subheader>Thumb-color</v-subheader>
 				<v-color-picker v-model="thumbColor"/>
 			</v-card>
