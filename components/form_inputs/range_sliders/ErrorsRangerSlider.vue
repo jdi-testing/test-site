c5f212b40feca409ef0fdac8ff29e3711f5c1641
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-11-10 12:48:40 +0300

    errors ready

--- components/form_inputs/range_sliders/ErrorsRangerSlider.vue
+++ components/form_inputs/range_sliders/ErrorsRangerSlider.vue
@@ -1,16 +1,15 @@
 <template>
 	<v-container>
 		<v-range-slider max="50" min="-50" :value="[-25, 25]" :error-messages="[defaultText1, defaultText2,defaultText3,defaultText4,defaultText5]" :error-count="errorCount" :hide-details="isDetailsHidden"/>
-			<v-card class="ma-2 pa-2">
-			    <v-switch label="Hide errors" v-model="isDetailsHidden" class="ma-0 ml-5"/>
-				<v-text-field label="Error count" v-model="errorCount" type="number" min="0" max="5"/>
-
-				<v-text-field v-model="defaultText1" label="Error messages 1"/>
-				<v-text-field v-model="defaultText2" label="Error messages 2"/>
-				<v-text-field v-model="defaultText3" label="Error messages 3"/>
-				<v-text-field v-model="defaultText4" label="Error messages 4"/>
-				<v-text-field v-model="defaultText5" label="Error messages 5"/>
-			</v-card>
+		<v-card class="ma-2 pa-2">
+			<v-switch label="Hide errors" v-model="isDetailsHidden" class="ma-0 ml-5"/>
+			<v-text-field label="Error count" v-model="errorCount" type="number" min="0" max="5"/>
+			<v-text-field v-model="defaultText1" label="Error messages 1"/>
+			<v-text-field v-model="defaultText2" label="Error messages 2"/>
+			<v-text-field v-model="defaultText3" label="Error messages 3"/>
+			<v-text-field v-model="defaultText4" label="Error messages 4"/>
+			<v-text-field v-model="defaultText5" label="Error messages 5"/>
+		</v-card>
 	</v-container>
 </template>
 <script>
--- components/form_inputs/range_sliders/LoadingRangerSlider.vue
+++ components/form_inputs/range_sliders/LoadingRangerSlider.vue
@@ -1,20 +1,18 @@
 <template>
-	<div>
-		<v-range-slider :dark="isDarkTheme" min="-50" max="50" :value="[-25, 25]" :loader-height="loaderHeight" loading
-		/>
-		<v-card class="ma-2">
-			<v-text-field label="Loader height" v-model="loaderHeight" type="number" max="500" min="0"/>
+	<v-container>
+		<v-card class="ma-2 pa-2">
+			<v-text-field label="Loader height" v-model="height" type="number" max="500" min="0"/>
 			<v-color-picker v-model="loadingColor"/>
 		</v-card>
-
-	</div>
+		<v-range-slider min="-50" max="50" :value="[-25, 25]" :loader-height="height" :loading="loadingColor"/>
+	</v-container>
 </template>
 <script>
 	export default {
 	name: 'LoadingRangerSlider',
 	data: () => ({
-	loaderHeight: 50,
-	loadingColor: red,
+	height: 10,
+	loadingColor: "Red",
 	}),
 	};
 </script>
