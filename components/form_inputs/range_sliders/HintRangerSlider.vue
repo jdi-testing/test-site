3b558ae44597fe7701e85e370edacc037ba34fad
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-11-10 13:53:00 +0300

    hint sepparete slider

---
+++ components/form_inputs/range_sliders/HintRangerSlider.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <v-range-slider
+      max="50"
+      min="-50"
+      :value="[-25, 25]"
+      hint="Some hint"
+    ></v-range-slider>
+  </v-container>
+</template>
