90762a0871a1ee1e70635328f9a3e218d16f53ce
Maiia_Afanaseva <Maiia_Afanaseva@epam.com>
2022-11-11 15:35:24 +0300

    4167_updateToolbarFix

--- components/bars/toolbars/VariousAttributesToolbar.vue
+++ components/bars/toolbars/VariousAttributesToolbar.vue
@@ -1,23 +1,18 @@
   <template>
     <div id="app">
-
         <v-card flat height=250>
           <v-container fluid>
             <v-row class="child-flex">
-              <div>
+              <v-col>
                 <v-toolbar rounded flat outlined short>
-
                   <v-btn
                     icon
                     class="hidden-xs-only"
                   >
                     <v-icon>mdi-arrow-left</v-icon>
                   </v-btn>
-
                   <v-toolbar-title>Rounded&Outlined&Short</v-toolbar-title>
-
                   <v-spacer></v-spacer>
-
                   <v-btn
                     icon
                     class="hidden-xs-only"
@@ -25,21 +20,19 @@
                     <v-icon>mdi-magnify</v-icon>
                   </v-btn>
                 </v-toolbar>
-              </div>
-
-              <div>
+              </v-col>
+              <v-col>
                 <v-toolbar dark shaped elevation="10" absolute>
                   <v-spacer></v-spacer>
                   <v-toolbar-title>Shaped&Elevation&Absolute</v-toolbar-title>
                   <v-btn icon>
                     <v-icon>mdi-reply</v-icon>
                   </v-btn>
-
                   <v-btn icon>
                     <v-icon>mdi-dots-vertical</v-icon>
                   </v-btn>
                 </v-toolbar>
-              </div>
+              </v-col>
             </v-row>
           </v-container>
         </v-card>
