aa3f709a5c3741fb603aa6a383a91cd2c39343fe
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-11-15 14:30:17 +0300

    label done

--- components/form_inputs/range_sliders/LabelRangerSlider.vue
+++ components/form_inputs/range_sliders/LabelRangerSlider.vue
@@ -3,6 +3,6 @@
 		<v-subheader>Label</v-subheader>
 		<v-range-slider max="50" min="-50" label="lableText" :value="[-25, 25]"/>
 		<v-subheader>Inverse label</v-subheader>
-		<v-range-slider max="50" min="-50" label="inverselableText" inverse-label:value="[-25, 25]"/>
+		<v-range-slider max="50" min="-50" label="inverselableText" :value="[-25, 25]" inverse-label/>
 	</v-container>
 </template>
