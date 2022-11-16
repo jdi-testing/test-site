44092b303484d5235113a691899ea0151d51bf9d
Georgy Pankratenko <georgy_pankratenko@epam.com>
2022-11-16 13:03:49 +0100

    4270 Extend `FooterPropsTable` with enabled pagination controls

--- components/tables/data_tables/FooterPropsTable.vue
+++ components/tables/data_tables/FooterPropsTable.vue
@@ -1,21 +1,51 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    :items-per-page="5"
-    item-key="name"
-    class="elevation-1"
-    :footer-props="{
-      showFirstLastPage: true,
-      firstIcon: 'mdi-arrow-collapse-left',
-      lastIcon: 'mdi-arrow-collapse-right',
-      prevIcon: 'mdi-minus',
-      nextIcon: 'mdi-plus',
-      disableItemsPerPage: true,
-      disablePagination: true,
-      showCurrentPage: true
-    }"
-  ></v-data-table>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <p class="text-h6">
+          Enabled pagination controls
+        </p>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          item-key="name"
+          class="elevation-1"
+          :footer-props="{
+            showFirstLastPage: true,
+            firstIcon: 'mdi-arrow-collapse-left',
+            lastIcon: 'mdi-arrow-collapse-right',
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus',
+            showCurrentPage: true
+          }"
+        ></v-data-table>
+      </v-col>
+
+      <v-col cols="12">
+        <p class="text-h6">
+          Disabled pagination controls
+        </p>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="5"
+          item-key="name"
+          class="elevation-1"
+          :footer-props="{
+            showFirstLastPage: true,
+            firstIcon: 'mdi-arrow-collapse-left',
+            lastIcon: 'mdi-arrow-collapse-right',
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus',
+            showCurrentPage: true,
+            disableItemsPerPage: true,
+            disablePagination: true,
+          }"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container> 
 </template>
 <script>
   export default {
