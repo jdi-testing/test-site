41d5f74e6759fd669db592ad095a5a1fb677ac1a
omega790 <prikhodko.olena@gmail.com>
2022-11-17 12:20:23 +0400

    4499 vuetify lazy: fix style

--- pages/Lazy.vue
+++ pages/Lazy.vue
@@ -3,7 +3,8 @@
         <v-col>
             <h1>Lazy</h1>
             <p class="body-1 mt-2">
-                The <code>v-lazy</code> component by default will not render its contents until it has been intersected. Scroll down and watch the element render as you go past it.
+                The <code>v-lazy</code> component by default will not render its contents until it has been intersected.
+                 Scroll down and watch the element render as you go past it.
             </p>
         </v-col>
 
