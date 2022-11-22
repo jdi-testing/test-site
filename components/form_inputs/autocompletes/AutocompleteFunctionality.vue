0731ff24911ea413f093f03db6d06691fb8fb0c1
pavelpostnikov1 <84065866+pavelpostnikov1@users.noreply.github.com>
2022-11-22 09:58:06 +0100

    Add files via upload

---
+++ components/form_inputs/autocompletes/AutocompleteFunctionality.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <h2>Functionality</h2>
+    <v-card>
+      <v-container>
+        <v-row align="center">
+          <v-col cols="40">
+            <v-autocomplete
+                    disabled
+                    dense
+                    v-model="values"
+                    :items="items"
+                    label="Disabled"
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="12">
+            <v-autocomplete
+                    clearable
+                    open-on-clear
+                    v-model="values"
+                    :items="items"
+                    dense
+                    label="Clearable, open on clear"
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="12">
+            <v-autocomplete
+                    auto-select-first
+                    v-model="values"
+                    :items="items"
+                    label="Autoselect first"
+                    dense
+            ></v-autocomplete>
+          </v-col>
+          <v-col cols="12">
+            <v-autocomplete
+                    v-model="values"
+                    :items="items"
+                    label="Small chips"
+                    multiple
+                    dense
+                    small-chips
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ['foo', 'bar', 'fizz', 'buzz'],
+    values: ['foo', 'bar'],
+    value: null,
+  }),
+};
+</script>
