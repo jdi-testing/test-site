399e77d796d0a0ce80bf3e6c67da468ad1453b33
Maiia_Afanaseva <Maiia_Afanaseva@epam.com>
2022-12-02 14:32:41 +0300

    4436_inputValidateOnBlurFeaturesUpdateTestSite

---
+++ components/form_inputs/inputs/ValidateOnBlurInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card
+    class="mx-auto"
+    style="max-width: 500px;"
+  >
+      <v-text-field
+        v-model="phone"
+        :rules="[rules.phone]"
+        validate-on-blur
+        filled
+        color="deep-purple"
+        label="Phone number"
+      ></v-text-field>
+  </v-card>
+</template>
+<script>
+export default {
+  data: () => ({
+    phone: undefined,
+    rules: {
+      phone: (v) => !!(v || '').match(/^\d+$/) || 'Please enter a valid phone number',
+    },
+  }),
+};
+</script>
