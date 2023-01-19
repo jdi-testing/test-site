b11e1971e29116a22a6e8d8f337b1df093a2fcf7
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2023-01-19 14:08:21 +0400

    Elements are added on the test-site DataIterators

---
+++ components/tables/data_iterators/NoDataText.vue
@@ -0,0 +1,108 @@
+<div id="app">
+  <v-app id="inspire">
+    <v-container fluid>
+      <v-data-iterator
+        :items="items"
+        :items-per-page.sync="itemsPerPage"
+        hide-default-footer
+        no-data-text="There's no any items data"
+      >
+        <template v-slot:header>
+          <v-toolbar
+            class="mb-2"
+            color="indigo darken-5"
+            dark
+            flat
+          >
+            <v-toolbar-title>This is a header</v-toolbar-title>
+          </v-toolbar>
+        </template>
+
+        <template v-slot:default="props">
+          <v-row>
+            <v-col
+              v-for="item in props.items"
+              :key="item.name"
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+            >
+              <v-card>
+                <v-card-title class="subheading font-weight-bold">
+                  {{ item.name }}
+                </v-card-title>
+
+                <v-divider></v-divider>
+
+                <v-list dense>
+                  <v-list-item>
+                    <v-list-item-content>Calories:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.calories }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Fat:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.fat }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Carbs:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.carbs }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Protein:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.protein }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Sodium:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.sodium }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Calcium:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.calcium }}
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-content>Iron:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      {{ item.iron }}
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-col>
+          </v-row>
+        </template>
+
+        <template v-slot:footer>
+          <v-toolbar
+            class="mt-2"
+            color="indigo"
+            dark
+            flat
+          >
+            <v-toolbar-title class="subheading">
+              This is a footer
+            </v-toolbar-title>
+          </v-toolbar>
+        </template>
+      </v-data-iterator>
+    </v-container>
+  </v-app>
+</div>
