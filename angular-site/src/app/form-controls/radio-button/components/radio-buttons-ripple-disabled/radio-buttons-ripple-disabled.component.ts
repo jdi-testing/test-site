2eac76a4fe912551c08aaa5cacb07740efe3e19e
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2023-07-04 18:47:13 +0300

    radio-buttons-added

---
+++ angular-site/src/app/form-controls/radio-button/components/radio-buttons-ripple-disabled/radio-buttons-ripple-disabled.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-buttons-ripple-disabled',
+  templateUrl: './radio-buttons-ripple-disabled.component.html',
+})
+export class RadioButtonsRippleDisabledComponent {
+
+}
--- angular-site/src/app/form-controls/radio-button/radio-buttons.module.ts
+++ angular-site/src/app/form-controls/radio-button/radio-buttons.module.ts
@@ -7,15 +7,20 @@ import {RadioNgModelExample} from "./components/radio-ng-model-example";
 import {RadioOverviewExample} from "./components/radio-overview-example";
 import {FormsModule} from "@angular/forms";
 import {MatRadioModule} from "@angular/material/radio";
+import { RadioButtonsDisabledComponent } from './components/radio-buttons-disabled/radio-buttons-disabled.component';
+import {MatButtonModule} from "@angular/material/button";
+import { RadioButtonsLabelPositionComponent } from './components/radio-buttons-label-position/radio-buttons-label-position.component';
+import { RadioButtonsRippleDisabledComponent } from './components/radio-buttons-ripple-disabled/radio-buttons-ripple-disabled.component';
 
 
 @NgModule({
-  declarations: [RadioButtonsComponent, RadioNgModelExample, RadioOverviewExample],
+  declarations: [RadioButtonsComponent, RadioNgModelExample, RadioOverviewExample, RadioButtonsDisabledComponent, RadioButtonsLabelPositionComponent, RadioButtonsRippleDisabledComponent],
   imports: [
     CommonModule,
     RadioButtonsRoutingModule,
     MatRadioModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
   ]
 })
 export class RadioButtonsModule { }
