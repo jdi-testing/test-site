6abc54f31aea8241ee8931bc5b37bf9243817cf5
Iulianna_Stepanova <iulianna_stepanova@epam.com>
2023-07-04 21:18:39 +0300

    button-toggle-added

--- angular-site/src/app/buttons-and-indicators/button-toggle/button-toggle.module.ts
+++ angular-site/src/app/buttons-and-indicators/button-toggle/button-toggle.module.ts
@@ -7,10 +7,14 @@ import {ButtonToggleOverviewExample} from './components/button-toggle-overview-e
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {ButtonToggleExclusiveExample} from './components/button-toggle-exclusive-example';
 import {MatIconModule} from '@angular/material/icon';
+import {ButtonToggleAppearanceComponent} from "./components/button-toggle-appearance/button-toggle-appearance.component";
+import { ButtonToggleDisabledComponent } from './components/button-toggle-disabled/button-toggle-disabled.component';
+import { ButtonToggleVerticalComponent } from './components/button-toggle-vertical/button-toggle-vertical.component';
+import { ButtonToggleDisableRippleComponent } from './components/button-toggle-disable-ripple/button-toggle-disable-ripple.component';
 
 
 @NgModule({
-  declarations: [ButtonToggleComponent, ButtonToggleOverviewExample, ButtonToggleExclusiveExample],
+  declarations: [ButtonToggleComponent, ButtonToggleOverviewExample, ButtonToggleExclusiveExample, ButtonToggleAppearanceComponent, ButtonToggleDisabledComponent, ButtonToggleVerticalComponent, ButtonToggleDisableRippleComponent],
   imports: [
     CommonModule,
     ButtonToggleRoutingModule,
---
+++ angular-site/src/app/buttons-and-indicators/button-toggle/components/button-toggle-disable-ripple/button-toggle-disable-ripple.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-toggle-disable-ripple',
+  templateUrl: './button-toggle-disable-ripple.component.html',
+})
+export class ButtonToggleDisableRippleComponent {
+
+}
