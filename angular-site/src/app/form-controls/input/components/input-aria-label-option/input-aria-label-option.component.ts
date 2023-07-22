44f1367678c96e4af755405575050100521c993a
Dzmitry Skurat <dzmitry_skurat@epam.com>
2023-07-22 19:01:59 +0300

    added input element with aria-label, readonly properties

---
+++ angular-site/src/app/form-controls/input/components/input-aria-label-option/input-aria-label-option.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'input-aria-label-option',
+  templateUrl: './input-aria-label-option.component.html'
+})
+export class InputAriaLabelOptionComponent {
+  value = 'Clear me';
+}
